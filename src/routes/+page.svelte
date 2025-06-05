9577948eb5319c44c50c30640bad26fc7d423ebc
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2025-06-05 12:01:01 +0100

    Added trailer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,20 @@
+<script>
+  const videoId = 'wHe7PtP9Lxk'; // YouTube's official embed test video
+</script>
+
 <title>Dance Brawler</title>
 
-<div class="pt-4 p-8">
-    <h1 class="pb-6 text-3xl font bold">Dance Brawler</h1>
-</div>
+<main class="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+	<div class="w-full max-w-3xl">
+		<div class="relative w-full pb-[56.25%] h-0">
+			<iframe
+			class="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
+			src={`https://www.youtube.com/embed/${videoId}`}
+			title="YouTube video player"
+			frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowfullscreen
+			></iframe>
+		</div>
+	</div>
+</main>
